c541f9794994d40b481d450f2f913f74d71f358e
Luis Fernando <luisfernandobrasilsantos@hotmail.com>
2024-01-20 16:47:08 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
   const buttons = document.querySelectorAll('[data-tab-button]');
+  const questions = document.querySelectorAll('[data-faq-question]');
+
+  const heroSection = document.querySelector('.hero');
+  const alturaHero = heroSection.clientHeight;
+
+  window.addEventListener('scroll', function() {
+    const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual < alturaHero) {
+      hiddenHeaderElements();
+    } else {
+      visibleHeaderElements();
+    }
+  })
+
+  function hiddenHeaderElements() {
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden');
+  }
   
+  function visibleHeaderElements() {
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden');
+  }
+
+  // Seção de Atrações - Programação das Abas
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function(button) {
       const abaAlvo = button.target.dataset.tabButton;
@@ -10,6 +35,11 @@ document.addEventListener('DOMContentLoaded', function() {
       removeBotaoAtivo();
       button.target.classList.add('shows__tabs__button--is-active');
     })
+  }
+
+  // Seção FAQ - Accordion
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener('click', abreOuFechaResposta);
   }
 })
 
@@ -27,4 +57,12 @@ function esconderAbas() {
   for (let i = 0; i < tabsContainer.length; i++) {
     tabsContainer[i].classList.remove('shows__list--is-active');
   }
+}
+
+function abreOuFechaResposta(elemento) {
+  const classe = 'faq__questions__item--is-open';
+  const elementoPai = elemento.target.parentNode;
+  console.log(elementoPai);
+
+  elementoPai.classList.toggle(classe);
 }
